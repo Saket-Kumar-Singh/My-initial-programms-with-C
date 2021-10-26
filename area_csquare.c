00eb3f5db5a14dcb01c142efcba2fef737baf0dd
Saket-Kumar-Singh <93042957+Saket-Kumar-Singh@users.noreply.github.com>
2021-10-26 16:06:23 +0530

    Add files via upload

---
+++ area_csquare.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+int a=5;
+int area;
+area=pow(a,2);
+printf("The value of area is %d",area);
+return 0;
+}
---
+++ sum_of_first_n_natural_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+int sum(int a);
+
+int main()
+{
+    int n;
+    printf("Give the number\n");
+    scanf("%d", &n);
+
+    printf("The sum of first %d numbers is %d", n, sum(n));
+    return 0;
+}
+int sum(int a)
+{
+    if (a == 0 || a == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return a + sum(a - 1);
+    }
+}
