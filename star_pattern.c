49c7269d63f549e5f8bd806dad9071cca48e5de5
Saket-Kumar-Singh <93042957+Saket-Kumar-Singh@users.noreply.github.com>
2021-10-26 19:55:58 +0530

    Add files via upload

---
+++ star_pattern.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int n;
+    int a = 0;
+    printf("No. of lines of stars needed\n");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++)
+    {
+        while (i > a)
+        {
+            printf("*");
+            a++;
+        }
+        if (i >= 1)
+        {
+            printf("\n");
+        }
+        // while (a >= 1)
+        // {
+        //     a--;
+        // }
+        a = 0;
+    }
+
+    return 0;
+}
