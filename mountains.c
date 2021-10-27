565d4ab036acfe4a98f1cbf5781c262ee8c075d9
Saket-Kumar-Singh <93042957+Saket-Kumar-Singh@users.noreply.github.com>
2021-10-28 00:08:17 +0530

    Add files via upload

--- mountains.c
+++ mountains.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
-void mountain(int a);
+//#include<math.h>
+//#include<stdlib.h>
+//#include<time.h>
+// void mountain(int a);
 int main()
 {
-    int i, n, b;
-    printf("No. of peaks you want");
-    scanf("%d", &n);
-    printf("OUTPUT:");
-    void mountain(n);
+    int a, b, c, d, n, i;
+    // printf("No. of peaks needed");
+    // scanf("%d",&n);
+    n = 3;
+    a = 1;
+    b = 0;
+    for (i = n; i > 0; i--)
+    {
+        while (b <= n)
+        {
+            while (i > a)
+            {
+                printf(" ");
+                a++;
+            }
+            while (a >= i)
+            {
+                printf("*");
+                if (a == n)
+                {
+                    break;
+                }
+                a++;
+            }
+            b++;
+            a = 0;
+        }
+
+        printf("\n");
+        b = 0;
+    }
+    // mountain(n);
 
     return 0;
 }
-void mountain(int a)
-{
-    int n, i;
-    int b = 0;
-    if (n == 1)
-    {
-        return;
-    }
-    mountain(n - 1);
-    for (i; i < n; i++)
-    {
-        if (i > b)
-        {
-            printf(" ");
-        }
-        else
-            printf("*");
-    }
-}
+// void mountain(int a)
+// {
+//     int n, i;
+//     if (n == 1)
+//     {
+//         return;
+//     }
+//     mountain(n - 1);
+//     for (i = 0; i < n; i++)
+//     {
+//         printf(" ");
+//     }
+//     printf("*");
+// }
